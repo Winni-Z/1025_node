48274bd9c67c38a34ccc0f8b62311e6464596311
tianyu <tianyu@gamil.com>
2019-01-23 09:40:06 +0800

    demo

--- day03/09.路由的使用/route.js
+++ day03/09.路由的使用/route.js
@@ -10,16 +10,35 @@ const PORT = 3000
 * request(请求对象上的常用方法)
 *     -request.query  get请求查询字符串的参数，拿到的是一个对象
 *     -request.params get请求路由的参数，拿到的是一个对象
-*     -request.body post请求体的参数，拿到的是一个对象
+*     -request.body post请求体的参数，拿到的是一个对象（借助一个中间件）
+*     -request.get() 获取请求头
 * response(响应对象)
+*     -response.send()    给浏览器做出一个响应
+*     -response.end()     给浏览器做出一个响应（容易乱码）
 *     -response.download  告诉浏览器下载一个文件
-*
+*     -response.sendFile 给浏览器发送一个文件（浏览器会自动打开）
+*     -response.redirect 重定向到一个新的地址（url）
+*     -response.set('demo',123)  自定义响应头内容
+*     -response.get() 获取响应头指定key对应的value（自定义的响应头一定能拿得到）
 * */
 
 //根路由
 app.get('/',(request,response)=>{
   // response.send('根路由返回的')
-  response.download('./music.mp3')
+  // response.end()
+  // response.download('./music.mp3')
+  // response.sendFile(__dirname+'/public/demo.html')
+  // response.sendFile(__dirname+'/public/demo.jpg')
+  // response.sendFile(__dirname+'/public/music.mp3')
+  // response.redirect('https://www.baidu.com')
+  // response.json({name:'peiqi',age:12})
+  // response.set('demo',123)
+  // const str = response.get('demo')
+  // console.log(str);
+
+  // const str = request.get('host')
+  // console.log(str);
+  // response.send('ok')
 })
 
 //一级路由
