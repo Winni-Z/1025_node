5e3148c7064bac3c63c97281ba38fb934bab563c
tianyu <tianyu@gamil.com>
2019-01-26 16:45:18 +0800

    个人中心

--- day06/06.cors跨域/ajaxServer.js
+++ day06/06.cors跨域/ajaxServer.js
@@ -3,15 +3,12 @@ const app = express()
 app.use(express.static('public'))
 app.use(express.urlencoded({extended:true}))
 
-
-app.get('/test',(req,res)=>{
-  const {callback} =  req.query
-  let arr = [{name:'peiqi',age:12},{name:'qiaozhi',age:13}]
-  const str = callback+'('+ JSON.stringify(arr) +')'
-  console.log('@@@@@@',str);
-  res.send(str)
+app.get('/testAjax',(req,res)=>{
+  console.log(req.query);
+  res.set('Access-Control-Allow-Origin', 'http://localhost:63342');
+  res.send('这是服务器端ajax——get的回应2')
+  // res.sendFile(__dirname+'/music.mp3')
 })
-
 
 
 app.listen(3000,(err)=>{
