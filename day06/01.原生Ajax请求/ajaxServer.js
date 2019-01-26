d0ee22d611f4ee56e7fca488428f4ff3d18290d5
tianyu <tianyu@gamil.com>
2019-01-26 11:50:56 +0800

    个人中心

--- day06/01.原生Ajax请求/ajaxServer.js
+++ day06/01.原生Ajax请求/ajaxServer.js
@@ -1,12 +1,18 @@
 const express = require('express')
 const app = express()
 app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
 
 app.get('/testAjax',(req,res)=>{
   console.log(req.query);
 
-  res.send('这是服务器端ajax——get的回应')
+  res.send('这是服务器端ajax——get的回应2')
   // res.sendFile(__dirname+'/music.mp3')
+})
+
+app.post('/testAjax2',(req,res)=>{
+  console.log(req.body);
+  res.send('这是服务器端ajax——post的回应')
 })
 
 
